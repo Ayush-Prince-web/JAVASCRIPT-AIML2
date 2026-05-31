bdd6a85564fe01731ac85a22648ce41bf887520e
Ayush kumar <ayushkumar2006@gmail.com>
2026-05-31 13:58:03 +0530

    Java

---
+++ Java.java
@@ -0,0 +1,11 @@
+package 
+
+public class Student{
+    public static void main (string[]args){
+        System.out.println("Heloo World");
+         System.out.println("AIML2");
+         int age =20;
+         System.out.println("age");
+         char 
+    }
+}
